721b14dbd546c838ce8d7f66e8ec20c53df05a36
Atap23 <antonioalvareztorres.trabajo@gmail.com>
2023-10-02 19:04:04 +0200

    DMWL :: General :: 0.1.0
    · Add missing files

---
+++ projects/collection-manager/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of collection-manager
+ */
+
+export * from './lib/collection-sort';
+export * from './lib/collection-filter';
+export * from './lib/collection-pagination';
+export * from './lib/collection-manager';
+export * from './lib/collection-manager.types';
